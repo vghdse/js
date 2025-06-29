cd90432ad6d784dde49687f3091774a95fe1e4a8
d33l <dgdarrell8@gmail.com>
2025-06-29 07:24:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     // ===== BOT CORE SETTINGS =====
     CHATBOT: getConfig("CHATBOT") || "on",
-    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~klwk47E4VAdpMiSsgOhM",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~YTSXewMl7zaWi78MjWtc",  // Your bot's session ID (keep it secure)
     PREFIX: getConfig("PREFIX") || ",",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "SUBZERO-MD",  // Bot's display name
     MODE: getConfig("MODE") || process.env.MODE || "public",        // Bot mode: public/private/group/inbox
