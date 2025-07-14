afa18e4b791db7850d8463759fd1ba5d131f08d0
d33l <dgdarrell8@gmail.com>
2025-07-14 02:17:26 +0200

    Update ac.js

--- plugins/ac.js
+++ plugins/ac.js
@@ -1,4 +1,4 @@
-const config = require('../config');
+/*const config = require('../config');
 const { cmd, commands } = require('../command');
 const os = require("os");
 const { runtime } = require('../lib/functions');
@@ -173,3 +173,4 @@ ${menuSections}
     }
 });
 
+*/
