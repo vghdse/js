6cdfe7a9afb7ed78f2a08ddcff997904587b94f3
d33l <dgdarrell8@gmail.com>
2025-07-14 02:13:48 +0200

    Update ac.js

--- plugins/ac.js
+++ plugins/ac.js
@@ -1,4 +1,4 @@
-/*const config = require('../config');
+const config = require('../config');
 const { cmd, commands } = require('../command');
 const os = require("os");
 const { runtime } = require('../lib/functions');
@@ -172,4 +172,4 @@ ${menuSections}
         reply(`❌ Error generating menu: ${e.message}`);
     }
 });
-*/
+
